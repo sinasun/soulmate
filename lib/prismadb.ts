5c0f53b6369f68d9e79639651bd3e0e062ff9642
Sina <sinakhodaveisi@gmail.com>
2023-01-28 15:12:07 -0500

    Camel case error database solved

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined
+	var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+console.log("Clinet:", client);
 
-export default client
-
+export default client;
