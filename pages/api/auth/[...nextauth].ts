d8fbf85d05bf0a63712bfacccbe095da6bd9387e
Sina <sinakhodaveisi@gmail.com>
2023-01-29 02:29:57 -0500

    removed email page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,45 +2,27 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import InstagramProvider from "next-auth/providers/instagram";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prismadb";
-import type { NextApiRequest, NextApiResponse } from "next";
 
-let email = "";
-function auth(
-	req: NextApiRequest,
-	res: NextApiResponse
-): ReturnType<typeof NextAuth> {
-	if (req.body.userType !== undefined) {
-		email = req.body.userType;
-		console.log(email);
-	}
-	return NextAuth(req, res, authOptions(req, res));
-}
-function authOptions(
-	req: NextApiRequest,
-	res: NextApiResponse
-): NextAuthOptions {
-	return {
-		adapter: PrismaAdapter(prisma),
-		secret: process.env.SECRET,
-		providers: [
-			InstagramProvider({
-				clientId: process.env.INSTAGRAM_CLIENT_ID,
-				clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
-				checks: [],
-			}),
-		],
-		theme: {
-			colorScheme: "light",
+export const authOptions: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	secret: process.env.SECRET,
+	providers: [
+		InstagramProvider({
+			clientId: process.env.INSTAGRAM_CLIENT_ID,
+			clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+			checks: [],
+		}),
+	],
+	theme: {
+		colorScheme: "light",
+	},
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			delete account?.user_id;
+			return true;
 		},
-		callbacks: {
-			async signIn({ user, account, profile, email, credentials }) {
-				console.log;
-				delete account?.user_id;
-				return true;
-			},
-		},
-		debug: true,
-	};
-}
+	},
+	debug: true,
+};
 
-export default auth;
+export default NextAuth(authOptions);
